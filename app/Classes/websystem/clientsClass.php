67ec2a5750470b33bdb8544d9ab5ade7117579fa
Cheza-Dzabala <dzabalamacheza@gmail.com>
2017-02-21 11:24:51 +0200

    Client Class Fix

--- app/Classes/websystem/clientsClass.php
+++ app/Classes/websystem/clientsClass.php
@@ -98,7 +98,7 @@ class clientsClass {
             $img->save($id_save_path, 60);
             return array($client_save_path, $id_save_path);
         }
-       // return array($client_save_path, $id_save_path);
+       return array($client_save_path, $id_save_path);
     }
 
     /**
